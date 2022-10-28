cbccf3d2ac68fd98410763658523d1a43e8e478e
Igor_PO <89515076779@ya.ru>
2022-10-28 21:07:25 +0300

    init: add bad themeChange

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,28 +1,67 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { GlobalSvgSelector } from '../../assets/images/icons/global/GlobalSvgSelector'
 import Select from 'react-select'
 import s from './Header.module.scss'
 
+
 interface Props {}
 
 export const Header = (props: Props) => {
+  const [theme, setTheme] = useState('light')
+
+  useEffect(() => {
+    const root = document.querySelector(':root') as HTMLElement;
+
+    const components = [
+      'body-background',
+      'components-background',
+      'card-background',
+      'card-shadow',
+      'text-color',
+    ]
+    components.forEach(component => {
+      root.style.setProperty(
+        `--${component}-default`,
+         `var(--${component}-${theme})`
+         );
+    })
+    root.style.setProperty(
+      '--body-background-default',
+       `var(--body-background-${theme})`
+       );
+
+  }, [theme])
+
+
+
   const options = [
     {value: 'city-1', label: 'Rostov-on-Don' },
     {value: 'city-2', label: 'Bataysk' },
     {value: 'city-3', label: 'Aksai' },
   ];
-
+  
   const colorStyles = {
     control: (styles: any) => ({
       ...styles,
-      backgroundColor: 'rgba(71, 147, 255, 0.2)',
+      backgroundColor: theme === 'dark' ? '#4f4f4f' :'rgba(71, 147, 255, 0.2)',
       width: '200px',
       height: '45px',
       border: 'none',
       borderRadius: '8px',
       zIndex: 100,
-    })
+    }),
+    singleValue: (styles: any) => ({
+      ...styles,
+      color: theme === 'dark' ? '#fff' : '#000',
+    }),
   }
+
+ 
+  function changeTheme(){
+    setTheme(theme === 'light'? 'dark' : 'light')
+  }
+
+  
   return (
     <header className={s.header}>
         <div className={s.wrapper}>
@@ -30,7 +69,7 @@ export const Header = (props: Props) => {
             <div className={s.title}>Weather Today</div>
         </div>
         <div className={s.wrapper}>
-            <div className={s.change_theme}>
+            <div className={s.change_theme} onClick={changeTheme}>
               <GlobalSvgSelector id="change-theme"/>
             
             </div>
